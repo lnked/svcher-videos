d0be607f7ebc97bdd7921c1ef7686e31f6b62fcb
Celebro dev <ed.proff@gmail.com>
2018-04-01 14:54:20 +0300

    update

--- public_html/core/classes/api.class.php
+++ public_html/core/classes/api.class.php
@@ -75,22 +75,6 @@ class Api
             'ed.proff@gmail.com' => 'ED CELEBRO'
         ];
 
-        // Create the Transport
-        $transport = (new Swift_SmtpTransport('smtp.softkor.ru', 465, 'ssl'))
-            ->setUsername('time@softkor.ru')
-            ->setPassword('1234567890')
-        ;
-
-        // Create the Mailer using your created Transport
-        $mailer = new Swift_Mailer($transport);
-
-        // Create a message
-        $message = (new Swift_Message($subject))
-            ->setFrom(['time@softkor.ru' => 'Софткор'])
-            ->setTo($emails)
-            ->setBody($body, 'text/html')
-        ;
-
         // // Create the message
         // $message = (new Swift_Message())
 
@@ -117,11 +101,40 @@ class Api
         //     Swift_Attachment::fromPath('/files/2018-03-28_15-42-10/video_1080p_2018_03_29_16_10_14.mp4')->setFilename('video.mp4')
         // );
 
-        if ($mailer->send($message)) {
-            $this->status = true;
-        } else {
-            $this->status = false;
-        }
+        // if ($mailer->send($message)) {
+        //     $this->status = true;
+        // } else {
+        //     $this->status = false;
+        // }
+
+        // $body = '<p>С уважением,<br>Администрация мероприятия</p>';
+
+        // $emails = [
+        //     'svcher@bk.ru' => 'Sergey Cher',
+        //     'ed.proff@gmail.com' => 'ED CELEBRO',
+        // ];
+
+        // // Create the Transport
+        // $transport = (new Swift_SmtpTransport('smtp.mail.ru', 465, 'ssl'))
+        //     ->setUsername('timefreeze23@softkor.ru')
+        //     ->setPassword('153426rhfy')
+        // ;
+
+        // // Create the Mailer using your created Transport
+        // $mailer = new Swift_Mailer($transport);
+
+        // // Create a message
+        // $message = (new Swift_Message('subject'))
+        //     ->setFrom(['timefreeze23@softkor.ru' => 'Софткор'])
+        //     ->setTo($emails)
+        //     ->setBody($body, 'text/html')
+        // ;
+
+        // $message->attach(
+        //     Swift_Attachment::fromPath(PATH_ROOT.DS.'files/2018-03-28_15-42-10/video_1080p_2018_03_29_16_10_14.mp4')->setFilename('video.mp4')
+        // );
+
+        // $mailer->send($message);
     }
 
     public function prepare($action)
--- public_html/init.php
+++ public_html/init.php
@@ -1,12 +1,37 @@
 <?php declare(strict_types = 1);
 
+error_reporting(E_ALL | E_STRICT);
+
 date_default_timezone_set('Europe/Moscow');
+
+define('LOCAL_SERVER', in_array($_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1')));
+define('DEV_MODE', LOCAL_SERVER);
 
 define('DS', DIRECTORY_SEPARATOR);
 define('PATH_ROOT', __DIR__);
 define('PATH_CORE', PATH_ROOT.DS.'core');
 define('PATH_VENDORS', PATH_CORE.DS.'vendors');
 define('PATH_TEMPLATE', PATH_CORE.DS.'template');
+
+define('DB_HOST', 'localhost');
+define('DB_BASE', 'video');
+define('DB_USER', 'root');
+define('DB_PASS', 'root');
+define('DB_PORT', '3306');
+define('DB_PREF', 'db');
+
+ini_set('display_errors', strval(intval(DEV_MODE)));
+ini_set('display_startup_errors', strval(intval(DEV_MODE)));
+ini_set('error_reporting', '32767');
+ini_set('fastcgi_read_timeout', '360');
+ini_set('request_terminate_timeout', '360');
+
+ini_set('session.auto_start', '0');
+ini_set('session.use_cookies', '1');
+ini_set('session.use_trans_sid', '0');
+ini_set('session.use_only_cookies', '1');
+ini_set('session.gc_maxlifetime', '2678400');
+ini_set('session.cookie_lifetime', '2678400');
 
 if (!session_id())
 {
